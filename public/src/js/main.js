985e1b51d8306b336cfad7175a4239ac3b6fdf50
Daniel Klein <danhirschklein@gmail.com>
2016-01-31 11:14:35 -0700

    Built Preliminary Get request with mapping function

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -7,40 +7,32 @@ $(document).on('ready', function() {
 
 
 //********NAZI VARIABLES***************\\
-var nytDateFilter = '';
+  var nytDateFilter = '';
+  var naziQuery
 
-var goebbels = new Nazi ('Joseph', 'Goebbels', 'Minister of Propaganda');
-var hitler = new Nazi ('Adolf', 'Hitler', 'Dictator of Germany');
-var himmler = new Nazi ('Heinrich', 'Himmler', 'Architect of Holocaust');
-//Place people/events/places in this array. Eventually should populate automatically via constructor.
+  var goebbels = new Nazi ('Joseph', 'Goebbels', 'Minister of Propaganda');
+  var hitler = new Nazi ('Adolf', 'Hitler', 'Dictator of Germany');
+  var himmler = new Nazi ('Heinrich', 'Himmler', 'Architect of Holocaust');
+  //Place people/events/places in this array. Eventually should populate automatically via constructor.
 
-var naziArr = [goebbels, hitler, himmler];
-var test = "";
-console.log (test)
-// var nytDateFilter = "";
+  var naziArr = [goebbels, hitler, himmler];
+
+
 //**********HELPER FUNCTIONS********\\
 //Finds the current date and converts it into a string that is takable by the NYT Search API
-function findDate() {
-  var date = new Date();
-var currentDay = date.getDate();
-var currentMonth = date.getMonth() + 1;
-var currentYear = date.getFullYear();
-var totalDate = currentYear.toString() + currentMonth.toString() + currentDay.toString();
 
-nytDateFilter = '&begin_date=' + totalDate;
-}
+
 
 
   //*******Creates Nazi or related term;
   function Nazi (firstName, lastName, naziBio) {
-this.nazi = firstName + " " + lastName;
-this.firstName = firstName;
-this.lastName = lastName;
-//Possibly pull Bio from wikipedia as stretch
-this.naziBio = naziBio;
-this.naziKeyWord = lastName
-
-  };
+    this.nazi = firstName + " " + lastName;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    //Possibly pull Bio from wikipedia as stretch
+    this.naziBio = naziBio;
+    this.naziKeyWord = lastName
+      };
 //********Creates Nazi related event
   function eventNazi (eventName, eventDesc) {
     this.eventName = eventName;
@@ -53,16 +45,51 @@ this.naziKeyWord = lastName
     this.place = place;
     this.placeDesc = placeDesc;
   }
-  //*********CREATE PRIMARY NYT SEARCH URL*****\\\\
-  // var nytUrl1 = nytLink + nytQuery1 + nytDeskFilter + nytDateFilter + nytAPIKey;
-  var nytLink = "http://api.nytimes.com/svc/search/v2/articlesearch.json?"
-  //**************Set Filters ********\\\
-  var nytQuery1 = "q=" + naziArr.forEach(function(e, index) {
-    return naziArr[index];
+
+   //**************Set Filters ********\\\
+  var nytLink = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+  //Set Desk Filter
+  var nytDeskFilter = '&fq=news_desk:("Politics")';
+  //Find Date
+  var date = new Date();
+  var currentDay = date.getDate();
+  var currentMonth = date.getMonth() + 1;
+  var currentYear = date.getFullYear();
+  var totalDate = currentYear.toString() + currentMonth.toString() + currentDay.toString();
+  var nytDateFilter = '&begin_date=' + totalDate;
   });
 
-  var nytDeskFilter = '&fq=news_desk:("Politics")';
-  findDate();
-
-
+naziArr.map(function(naziKey) { $.ajax({
+  url: nytLink + naziKey + nytDeskFilter + nytDateFilter + nytAPIKey,
+  method: "GET",
+    success: function(date) {
+        if !!(data["response"]["meta"]["hits"]) {
+         naziHit.push(data["response"]["docs"])
+         counter = 0;
+        }
+        else {
+          counter++;
+        }
+      })
+    )}
+  });
 });
+
+//Preliminary Search
+//1)Set up a looping Ajax request.
+
+//2) On each loop passes a keyword from the naziArr array (naziArr[i].naziKeyWord) into the search url and run the AJAX request.
+
+//3)Within the Ajax request set up this if statement: if (data["response"]["meta"]["hits"] !== 0) { naziHit.push(naziArr[i]) }
+
+//4 ) if at the end of the prelimary search naziHit.length ===0, then program ends and counter goes up one.
+
+//5 NEW) If there is a hit, push the json object into an array.
+
+//************Secondary Search
+// Originally the idea was to take the naziHit array, and rerun it through the NYT ajax request with keywords from the Politican List API. This was dumb. Instead just hold onto the JSON hits from search 1 and send the Politican List searching through those. Obviously.
+
+//
+
+
+
