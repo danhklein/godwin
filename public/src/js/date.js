a5e0415a57920487b493901d43f9379b73d4a093
Daniel Klein <danhirschklein@gmail.com>
2016-03-26 13:18:47 -0600

    Removed console.log instances

--- public/src/js/date.js
+++ public/src/js/date.js
@@ -19,8 +19,6 @@ function DaysSinceHit (arr){
   var secondDate = new Date(currentYear,currentMonth,currentDay);
 
   var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
-  console.log('RecentDate: ',firstDate);
-  console.log(diffDays);
   $('#counter').text(diffDays);
   if (diffDays === 0) {
     $('#counter').css('color', 'red');
@@ -53,24 +51,3 @@ function DaysSinceHit (arr){
   // var testDate = "&begin_date=20150101";
 
 
-// function DaysSinceHit (arr){
-//   var oneDay = 24*60*60*1000; // hours*minutes*seconds*millisecondsvar date = new Date();
-//   var currentDay = date.getDate();
-//   var currentMonth = date.getMonth();
-//   var currentYear = date.getFullYear();
-//   var recentDay, recentMonth, recentYear;
-//   recentYear = parseInt(arr[0].pub_date.substring(0,4));
-//   recentMonth = parseInt(arr[0].pub_date.substring(5,7))-1;
-//   recentDay = parseInt(arr[0].pub_date.substring(8,10));
-
-//   var firstDate = new Date(recentYear,recentMonth,recentDay);
-//   var secondDate = new Date(currentYear,currentMonth,currentDay);
-
-//   var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
-//   console.log('RecentDate: ',firstDate);
-//   console.log(diffDays);
-//   $('#counter').text(diffDays);
-//   if (diffDays === 0) {
-//     $('#counter').css('color', 'red')
-//   }
-// }
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -31,53 +31,7 @@
   }
 
 
-//**********HELPER FUNCTIONS********\\
-//Finds the current date and converts it into a string that is takable by the NYT Search API
 
-
-
-  //  //**************Set Filters ********\\\
-  // var nytLink = "http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?q="
-  // //Set Desk Filter
-  // var nytCallback = "&callback=svc_search_v2_articlesearch"
-  // var nytDeskFilter = '&fq=news_desk:("Politics")';
-  // //Find Date
-  // var nytAPIKey = "&api-key=534e57e32a30382b3b6da874e8f42d3a:5:71918911";
-  // var naziKeyarr = naziArr.naziKeyWord;
-
-  // var date = new Date();
-  // var currentDay = date.getDate();
-  // var currentMonth = date.getMonth();
-  // var currentYear = date.getFullYear();
-  // var recentDay, recentMonth, recentYear;
-
-  // var totalDate = currentYear.toString() + "0"+ currentMonth.toString() + "0" +currentDay.toString();
-  // var nytDateFilter = '&begin_date=' + totalDate;
-
-  // var testDate = "&begin_date=20150101";
-
-
-// function DaysSinceHit (arr){
-//   var oneDay = 24*60*60*1000; // hours*minutes*seconds*millisecondsvar date = new Date();
-//   var currentDay = date.getDate();
-//   var currentMonth = date.getMonth();
-//   var currentYear = date.getFullYear();
-//   var recentDay, recentMonth, recentYear;
-//   recentYear = parseInt(arr[0].pub_date.substring(0,4));
-//   recentMonth = parseInt(arr[0].pub_date.substring(5,7))-1;
-//   recentDay = parseInt(arr[0].pub_date.substring(8,10));
-
-//   var firstDate = new Date(recentYear,recentMonth,recentDay);
-//   var secondDate = new Date(currentYear,currentMonth,currentDay);
-
-//   var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
-//   console.log('RecentDate: ',firstDate);
-//   console.log(diffDays);
-//   $('#counter').text(diffDays);
-//   if (diffDays === 0) {
-//     $('#counter').css('color', 'red')
-//   }
-// }
 
   var naziHit = [];
 
@@ -114,8 +68,6 @@ $('#naziButton').on('click', function ()
     makeAjax(url3);
     $('#topic').text('Nazis');
     $('#intro').text(nazi.eventDesc);
-    console.log('buttontest', diffDays)
-
   });
 $('#hitlerButton').on('click', function ()
   { $("#first").empty();
@@ -134,8 +86,6 @@ $('#fascismButton').on('click', function ()
     makeAjax(url4);
     $('#topic').text('Fascism');
     $('#intro').text(fascism.eventDesc);
-    console.log('buttontest', diffDays)
-
   });
 
 });
@@ -169,7 +119,6 @@ function getNewest (arr) {
         }
     };
     arr[0] = newest;
-    console.log(newest.pub_date);
 }
 
 function recentPop(arr) {
